99e594331f4530df34c1073ce9c8d5516f980747
haohong11 <79410153@qq.com>
2017-10-06 18:25:00 +0800

    Update menu.c

--- lab2/menu.c
+++ lab2/menu.c
@@ -2,20 +2,44 @@
 #include <stdlib.h>
 
 int main(){
-	char cmd[128];
-	while(1){
-	scanf("%s",cmd);
-	if(!strcmp(cmd,"help"))
-	{
-		printf("This is help cmd\n");
-	}
-	else if(!strcmp(cmd,"quit"))
-	{
-		exit(0);
-	}
-	else
-	{
-		printf("Wrong cmd!\n");
-	}
-	}
+    char cmd[128];
+    while(1){
+        scanf("%s",cmd);
+        if(!strcmp(cmd,"help"))
+        {
+            printf("This is help cmd\n");
+        }
+        else if(!strcmp(cmd,"A"))
+        {
+            printf("a");
+        }
+        else if(!strcmp(cmd,"B"))
+        {
+            printf("b");
+        }
+        else if(!strcmp(cmd,"C"))
+        {
+            printf("c");
+        }
+        else if(!strcmp(cmd,"D"))
+        {
+            printf("d");
+        }
+        else if(!strcmp(cmd,"E"))
+        {
+            printf("e");
+        }
+        else if(!strcmp(cmd,"F"))
+        {
+            printf("f");
+        }
+        else if(!strcmp(cmd,"quit"))
+        {
+            exit(0);
+        }
+        else
+        {
+            printf("Wrong cmd!\n");
+        }
+    }
 }
